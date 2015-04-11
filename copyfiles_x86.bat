1da5aa3fc672e4a3a50bba6aa400439b22c52faf
Ake Hedman, Paradise of the Frog <akhe@grodansparadis.com>
2015-04-11 16:05:50 +0200

    Working with release

--- copyfiles_x86.bat
+++ copyfiles_x86.bat
@@ -4,3 +4,8 @@ copy ..\vscp_software\src\vscp\vscpworks\release\vscpworks.exe files\x86
 copy ..\vscp_software\src\vscp\daemon\win32\release\vscpd.exe files\x86
 copy ..\vscp_software\src\vscp\vscpcmd\release\vscpcmd.exe files\x86
 copy ..\vscp_software\src\vscp\mkpasswd\release\mkpasswd.exe files\x86
+
+REM Development tools
+copy "..\vscp_software\src\vscp\helperlib\windows\release\vscphelper.dll" files\system\x86
+copy "..\vscp_software\src\vscp\helperlib\windows\release\vscphelper.dll" files\lib\x86
+copy "..\vscp_software\src\vscp\helperlib\windows\release\vscphelper.lib" files\lib\x86
